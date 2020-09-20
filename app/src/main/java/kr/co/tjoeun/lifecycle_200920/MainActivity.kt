e6744516d9c1e7b3bb12410ea860586b2addd3eb
JunSeung Mo <mojuns@gmail.com>
2020-09-20 15:15:31 +0900

    Initial Commit

---
+++ app/src/main/java/kr/co/tjoeun/lifecycle_200920/MainActivity.kt
@@ -0,0 +1,14 @@
+package kr.co.tjoeun.lifecycle_200920
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("메인화면", "onCreate 실행됨")
+    }
+}
